4d5544433f453df0db329475bc0e8c518cbe3d90
Blahpr <tendonen7@gmail.com>
2024-09-14 22:46:29 -0700

    Delete EnterpriseDT.Util.Debug directory

--- EnterpriseDT.Util.Debug/CustomLogLevelAppender.cs
+++
@@ -1,6 +0,0 @@
-namespace EnterpriseDT.Util.Debug;
-
-public interface CustomLogLevelAppender : Appender
-{
-    Level CurrentLevel { get; set; }
-}
--- EnterpriseDT.Util.Debug/LogMessageHandler.cs
+++
@@ -1,3 +0,0 @@
-namespace EnterpriseDT.Util.Debug;
-
-public delegate void LogMessageHandler(object sender, LogMessageEventArgs e);
