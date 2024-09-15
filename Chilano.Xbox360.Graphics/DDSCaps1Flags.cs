6a8a5c7fd6ead7ff0a0830b950179912aacde6bf
Blahpr <tendonen7@gmail.com>
2024-09-14 22:45:37 -0700

    Delete Chilano.Xbox360.Graphics directory

--- Chilano.Xbox360.Graphics/DDSCaps1Flags.cs
+++
@@ -1,8 +0,0 @@
-namespace Chilano.Xbox360.Graphics;
-
-public enum DDSCaps1Flags : uint
-{
-    DDSCAPS_COMPLEX = 8u,
-    DDSCAPS_TEXTURE = 0x1000u,
-    DDSCAPS_MIPMAP = 0x400000u
-}
--- Chilano.Xbox360.Graphics/XPRFormat.cs
+++
@@ -1,8 +0,0 @@
-namespace Chilano.Xbox360.Graphics;
-
-public enum XPRFormat : byte
-{
-    DXT1 = 12,
-    ARGB = 6,
-    None = 0
-}
